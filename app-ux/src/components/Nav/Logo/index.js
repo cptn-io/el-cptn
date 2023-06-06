b561f4e67e65e4e56eb6aa8321a82a917616004e
KC <kcthota@gmail.com>
2023-06-06 14:00:18 -0700

    Logo changes in product. Ref #133

--- app-ux/src/components/Nav/Logo/index.js
+++ app-ux/src/components/Nav/Logo/index.js
@@ -2,9 +2,7 @@ import { Link } from "react-router-dom";
 
 const Logo = ({ className }) => {
     return <Link to={window.location.pathname.startsWith("/app") ? "/home" : "/"} aria-current="page" aria-label="Homepage" className={`flex-0 btn btn-ghost px-2 ${className}`}>
-        <div className="font-title text-primary inline-flex text-3xl">
-            <span className="lowercase">CPTN</span> <span className="text-base-content lowercase">.io</span>
-        </div>
+        <img alt="Logo" src="/logo.svg" className="nav-logo" />
     </Link>
 }
 
