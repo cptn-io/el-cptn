166071a996ffdcc14a3a2fd81f4d55333270bddb
KC <kcthota@gmail.com>
2023-06-08 23:39:53 -0700

    update logo height

--- app-ux/src/components/Nav/Logo/index.js
+++ app-ux/src/components/Nav/Logo/index.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 const Logo = ({ className }) => {
-    return <Link to={window.location.pathname.startsWith("/app") ? "/home" : "/"} aria-current="page" aria-label="Homepage" className={`flex-0 btn btn-ghost px-2 ${className}`}>
+    return <Link to={window.location.pathname.startsWith("/app") ? "/home" : "/"} aria-current="page" aria-label="Homepage" className={`flex-0 btn btn-ghost px-2 ${className || ''}`}>
         <img alt="Logo" src="/logo.svg" className="nav-logo" />
     </Link>
 }
