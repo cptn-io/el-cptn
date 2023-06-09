efd4ead58fb269374c127970bf4dafb6e18dee1e
KC <kcthota@gmail.com>
2023-06-09 00:53:19 -0700

    Switch logo based on theme

--- app-ux/src/components/Nav/Logo/index.js
+++ app-ux/src/components/Nav/Logo/index.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 const Logo = ({ className }) => {
     return <Link to={window.location.pathname.startsWith("/app") ? "/home" : "/"} aria-current="page" aria-label="Homepage" className={`flex-0 btn btn-ghost px-2 ${className || ''}`}>
         <img alt="Logo" src="/logo.svg" className="nav-logo" />
+        <img alt="Logo" src="/logo-darkbg.svg" className="nav-logo darkbg" />
     </Link>
 }
 
